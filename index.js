2260775c302f171b4d1491fbfe2b83eda5151424
Connor Shea <connor.james.shea@gmail.com>
2020-01-12 17:43:36 -0700

    Change the box values to an object.

--- index.js
+++ index.js
@@ -7,12 +7,14 @@ if (typeof AFRAME === 'undefined') {
 const VGLIST_URL = "https://vglist.co";
 // const VGLIST_URL = "http://localhost:3000";
 
-BOX_HEIGHT = 2.5;
-BOX_WIDTH = 1.5;
-BOX_DEPTH = 0.1;
-BOX_MARGIN = 0.3;
-BOX_HEIGHT_OFF_GROUND = 0.75;
-BOX_Z_POSITION = -4;
+GAME_BOX = {
+  height: 2.5,
+  width: 1.5,
+  depth: 0.1,
+  margin: 0.3,
+  height_off_ground: 0.75,
+  z_position: -4
+}
 
 /**
  * Stupid vglist VR Viewer component for A-Frame.
@@ -37,7 +39,7 @@ AFRAME.registerComponent('vglist-vr-viewer', {
 
     let libraryTextEl = document.createElement('a-entity');
     libraryTextEl.setAttribute('text', `color: black; width: 5; wrap-count: 15; align: center; side: double; value: ${username}'s Library`);
-    libraryTextEl.setAttribute('position', { x: 0.5, y: BOX_HEIGHT + 2, z: BOX_Z_POSITION });
+    libraryTextEl.setAttribute('position', { x: 0.5, y: GAME_BOX.height + 2, z: GAME_BOX.z_position });
     sceneEl.appendChild(libraryTextEl);
 
     let xPosition = -10;
@@ -61,29 +63,29 @@ AFRAME.registerComponent('vglist-vr-viewer', {
       entityEl.setAttribute('src', `#img${i}`);
       entityEl.setAttribute('geometry', {
         primitive: 'box',
-        height: BOX_HEIGHT,
-        width: BOX_WIDTH,
-        depth: BOX_DEPTH
+        height: GAME_BOX.height,
+        width: GAME_BOX.width,
+        depth: GAME_BOX.depth
       });
-      entityEl.setAttribute('position', { x: xPosition, y: BOX_HEIGHT / 2 + 0.25, z: BOX_Z_POSITION });
+      entityEl.setAttribute('position', { x: xPosition, y: GAME_BOX.height / 2 + 0.25, z: GAME_BOX.z_position });
       sceneEl.appendChild(entityEl);
 
       // Create a fake shadow below each box, much simpler than messing with lighting stuff.
       let fakeShadow = document.createElement('a-plane');
-      fakeShadow.setAttribute('position', { x: xPosition, y: 0.01, z: BOX_Z_POSITION });
+      fakeShadow.setAttribute('position', { x: xPosition, y: 0.01, z: GAME_BOX.z_position });
       fakeShadow.setAttribute('rotation', '-90 0 0');
-      fakeShadow.setAttribute('width', BOX_WIDTH);
-      fakeShadow.setAttribute('height', BOX_DEPTH);
+      fakeShadow.setAttribute('width', GAME_BOX.width);
+      fakeShadow.setAttribute('height', GAME_BOX.depth);
       fakeShadow.setAttribute('color', '#000');
       fakeShadow.setAttribute('opacity', 0.25);
       sceneEl.appendChild(fakeShadow);
 
       let textEl = document.createElement('a-entity');
-      textEl.setAttribute('text', `color: black; width: ${BOX_WIDTH}; wrap-count: 15; baseline: bottom; align: center; side: double; value: ${gamePurchase['game']['name']}`);
-      textEl.setAttribute('position', { x: xPosition, y: BOX_HEIGHT + 0.5, z: BOX_Z_POSITION });
+      textEl.setAttribute('text', `color: black; width: ${GAME_BOX.width}; wrap-count: 15; baseline: bottom; align: center; side: double; value: ${gamePurchase['game']['name']}`);
+      textEl.setAttribute('position', { x: xPosition, y: GAME_BOX.height + 0.5, z: GAME_BOX.z_position });
       sceneEl.appendChild(textEl);
 
-      xPosition += BOX_WIDTH + BOX_MARGIN;
+      xPosition += GAME_BOX.width + GAME_BOX.margin;
     });
 
     sceneEl.appendChild(assetsEl);
